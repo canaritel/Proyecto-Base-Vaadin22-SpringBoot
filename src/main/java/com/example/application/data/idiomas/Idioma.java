201ef21b57e1debb8d53e7bcddece36f045ca98a
Antonio Gonzalez <toni@wificlick.es>
2021-12-24 15:14:38 +0000

    Creamos los archivos properties para la traducción

---
+++ src/main/java/com/example/application/data/idiomas/Idioma.java
@@ -0,0 +1,34 @@
+package com.example.application.data.idiomas;
+
+import java.io.IOException;
+import java.util.Properties;
+
+public class Idioma extends Properties {
+
+    Properties properties = new Properties();
+
+    public Idioma(String idioma) {
+        // Modificar si quieres añadir mas idiomas
+        // Cambia el nombre de los ficheros o añade los necesarios
+        switch (idioma) {
+            case "Spain":
+                getProperties("/traducciones/spanish.properties");
+                break;
+            case "English":
+                getProperties("/traducciones/english.properties");
+                break;
+            default:
+                getProperties("/traducciones/spanish.properties");
+        }
+
+    }
+
+    private void getProperties(String idioma) {
+        try {
+            this.load(getClass().getResourceAsStream(idioma));
+        } catch (IOException ex) {
+
+        }
+    }
+
+}
